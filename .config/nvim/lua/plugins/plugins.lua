6b372482cded73f550478639d3003e97824d98ce
aman <amank98@gmail.com>
2023-05-06 13:53:35 -0400

    trying again

---
+++ .config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,56 @@
+local ensure_packer = function()
+	local fn = vim.fn
+	local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+		vim.cmd [[packadd packer.nvim]]
+		return true
+	end
+	return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+
+	-- dev-icons used for lualine and nvm-tree
+	use 'nvim-tree/nvim-web-devicons'
+	use 'nvim-lualine/lualine.nvim'
+	use 'nvim-tree/nvim-tree.lua'
+
+	-- theme
+	use 'folke/tokyonight.nvim'
+
+	-- telescope grepping
+	use {
+		'nvim-telescope/telescope.nvim', tag = '0.1.1',
+		requires = { {'nvim-lua/plenary.nvim'} }
+	}
+
+	-- treesitter
+	use {
+		'nvim-treesitter/nvim-treesitter',
+		run = function()
+			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			ts_update()
+		end,
+	}
+
+	-- nvim-lspconfig for quick lsp configurations
+	use 'neovim/nvim-lspconfig'
+	use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+	use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+	use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+	use 'L3MON4D3/LuaSnip' -- Snippets plugin
+
+	-- used for git
+	use 'tpope/vim-fugitive'
+
+	-- for nice tabs
+	use {'akinsho/bufferline.nvim', tag = "v3.*", requires = 'nvim-tree/nvim-web-devicons'}
+
+	if packer_bootstrap then
+		require('packer').sync()
+	end
+end)
