27e1d5f8e71cbcbb5022f16535bbf6458152a1ea
aman <amank98@gmail.com>
2023-12-09 02:59:11 -0500

    add shorcut to organize python imports

--- .config/nvim/lua/plugins/configs/init.lua
+++ .config/nvim/lua/plugins/configs/init.lua
@@ -2,5 +2,6 @@ require('plugins.configs.lualine')
 require('plugins.configs.nvim-lspconfig')
 require('plugins.configs.nvim-tree')
 require('plugins.configs.nvim-treesitter')
+require('plugins.configs.pyright')
 require('plugins.configs.telescope')
 
---
+++ .config/nvim/lua/plugins/configs/pyright.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_set_keymap("n", "<leader>oi", "<CMD>PyrightOrganizeImports<CR>", {})
+
