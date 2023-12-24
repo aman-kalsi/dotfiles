bba8c38dc942f357e8fa6d34bbaaead8d98763a2
aman <amank98@gmail.com>
2023-12-24 01:07:52 -0500

    comment

--- .config/nvim/lua/plugins/configs/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/configs/nvim-treesitter.lua
@@ -1,6 +1,5 @@
 -- treesitter
 require('nvim-treesitter.configs').setup {
-	-- A list of parser names, or "all" (the five listed parsers should always be installed)
 	ensure_installed = { "vim", "lua", "javascript", "typescript", "python", "ocaml" },
 	highlight = {
 		enable = true,
