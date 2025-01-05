10ae7a0ff41f357375c9ac356b61f7eadae75031
aman <amank98@gmail.com>
2025-01-05 13:52:17 -0500

    fix clipboard config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,14 +2,16 @@ require('global_settings')
 require('plugins')
 
 -- Enable copying from WSL to windows clipboard - need win32yank.exe in usr/local/bin folder with x permission for this to work properly.
+vim.opt.clipboard:append("unnamedplus")
 vim.g.clipboard = {
     name = "win32yank-wsl",
     copy = {
-        ["*"] = "win32yank.exe -i --crlf"
+        ["*"] = "win32yank.exe -i --crlf",
+        ["+"] = "win32yank.exe -i --crlf",
     },
     paste = {
-        ["*"] = "win32yank.exe -o --lf"
+        ["*"] = "win32yank.exe -o --lf",
+        ["+"] = "win32yank.exe -o --lf",
     },
     cache_enabled = 0,
 }
-
